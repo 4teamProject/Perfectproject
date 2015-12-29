620437e609c715aead13ef8362c66aaf3d41d3ce
YongHChoi <crimenut@gmail.com>
2015-12-29 09:56:35 +0900

    create project create Hello Git

---
+++ Hello Git/src/com/sist/HelloGit.java
@@ -0,0 +1,13 @@
+package com.sist;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Git!");
+	}
+
+}
+
+
+// add index
