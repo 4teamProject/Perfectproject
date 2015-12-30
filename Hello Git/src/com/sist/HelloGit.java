828b5cabab6ff3fecf94714748f5ee45bb13d526
YongHChoi <crimenut@gmail.com>
2015-12-30 09:26:45 +0900

    added print line

--- Hello Git/src/com/sist/HelloGit.java
+++ Hello Git/src/com/sist/HelloGit.java
@@ -5,9 +5,13 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello Git!");
+        System.out.println("What's git?");
+        
 	}
 
 }
 
 
 // add index
+// commit
+// 노란 원통 아이콘 : 추적중임.
