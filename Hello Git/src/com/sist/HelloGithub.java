4a0f291f0fd7c2f89be9a0072b8c9bfe88cb53d6
YongHChoi <crimenut@gmail.com>
2016-01-14 10:10:48 +0900

    remote repository add a HelloGitHub.java

---
+++ Hello Git/src/com/sist/HelloGithub.java
@@ -0,0 +1,10 @@
+package com.sist;
+
+public class HelloGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("2016_0114_1000");
+	}
+
+}
