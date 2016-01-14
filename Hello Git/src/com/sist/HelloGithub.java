cd2bfc963e19a968a5869bffc8d3fc7510092369
YongHChoi <crimenut@gmail.com>
2016-01-14 10:30:01 +0900

    "2016_0114_1029"

--- Hello Git/src/com/sist/HelloGithub.java
+++ Hello Git/src/com/sist/HelloGithub.java
@@ -5,6 +5,7 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("2016_0114_1000");
+         System.out.println("2016_0114_1028");
 	}
 
 }
